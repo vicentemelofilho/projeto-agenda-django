b48d5d469ddb76e1a0445e5d114ba4571988f916
vicentemelofilho <vicentemelofilho@gmail.com>
2024-08-20 01:16:59 -0300

    489. Usando UserCreationForm para criar novos usuários no Django

--- project/settings.py
+++ project/settings.py
@@ -1,3 +1,5 @@
+# type: ignore
+# flake8: noqa
 """
 Django settings for project project.
 
@@ -110,7 +112,7 @@ LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'America/Sao_Paulo'
 
-USE_I18N = False
+USE_I18N = True
 
 USE_TZ = True
 
